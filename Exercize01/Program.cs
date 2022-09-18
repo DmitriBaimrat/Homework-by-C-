c902861a828760a3f425b8cd1db31a38db432652
Dmitri Baimrat <baimratus@mail.ru>
2022-09-18 11:30:51 +0500

    Exercize № 1

---
+++ Exercize01/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine("Первое число " + a + " больше второго числа " + b);
+}
+else if (a < b)
+{
+    System.Console.WriteLine("Первое число " + a + " меньше второго числа "+ b);
+}
+else
+{
+    System.Console.WriteLine("Первое число равно второму числу");
+}
