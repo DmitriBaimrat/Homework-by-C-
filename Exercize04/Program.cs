1f306c04f00b6f5fd32c0f9bd304cb27c11c4e05
Dmitri Baimrat <baimratus@mail.ru>
2022-09-18 13:40:13 +0500

    Exercize №4

---
+++ Exercize04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int num = 1;
+
+while (num <= N)
+{
+  if (num % 2 == 0)
+  {
+    System.Console.Write(num + ", ");
+  }  
+  num++;
+}
